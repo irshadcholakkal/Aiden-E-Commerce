727f38ea9a0a35ec886fc09adf84da3d3ce90b50
irshadcholakkal <irshadsh12773@gmail.com>
2023-11-06 14:14:19 +0530

    chechit

--- lib/View/SplashScreen/splashScreen.dart
+++ lib/View/SplashScreen/splashScreen.dart
@@ -15,23 +15,26 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 4), () { 
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>const ContentsOfSplashScreen() ,));
-    });
-  }
+  // void initState() {
+  //   super.initState();
+  //   Timer(const Duration(seconds: 4), () { 
+  //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>const ContentsOfSplashScreen() ,));
+  //   });
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Lottie.asset("Assets/infinity.json",height:20),
+          SizedBox(
+            height: MediaQuery.of(context).size.height*0.10,
+          ),
           Center(
             child: brandName(30.00, Colors.black, FontWeight.w500)
             
-          )
+          ),
+          Lottie.asset("Assets/infinity.json",height:50)
         ],
       ),
     );
