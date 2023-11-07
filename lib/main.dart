25d3b8ee14209873ce67d28249bee458b5785e54
irshadcholakkal <irshadsh12773@gmail.com>
2023-11-07 15:13:15 +0530

    pushing textfiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 
 
-<<<<<<< HEAD
-=======
-import 'package:aiden/view/login_screen/login_screen.dart';
-import 'package:aiden/view/signup_page/signup_page.dart';
->>>>>>> ee7384d6d09b3e5501c37b09151cdaa480d7c848
 import 'package:aiden/view/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -19,11 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return   const MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
       home: SplashScreen()
-=======
-      home: Loginscreen()
->>>>>>> ee7384d6d09b3e5501c37b09151cdaa480d7c848
     );
   }
 }
