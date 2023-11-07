99300e9ad98238796402112d985dfd00e12499f2
mohammedakbark <akbarmohd00123@gmail.com>
2023-11-07 21:03:57 +0530

    akbare dd

--- lib/utils/texts.dart
+++ lib/utils/texts.dart
@@ -1,35 +1,31 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Text firstSlideTextMain =Text("20% Discount New Arrival Products",
-style: GoogleFonts.rubik(
-  fontSize: 30,
-  fontWeight: FontWeight.w500
-),);
-Text secondSlideTextMain =Text("20% Discount New Arrival Products",
-style: GoogleFonts.rubik(
-  fontSize: 30,
-  fontWeight: FontWeight.w500
-),);
-Text thirdSlideTextMain =Text("20% Discount New Arrival Products",
-style: GoogleFonts.rubik(
-  fontSize: 30,
-  fontWeight: FontWeight.w500
-),);
-Text firstSlideTextsub =Text("Revamp your style with our latest arrivals! Enjoy an exclusive 20% off. Elevate your wardrobe with trendy, high-quality pieces. From fashion-forward apparel to the perfect accessories, we have it all. Limited time offer. Shop now and be part of the fashion revolution!",
-style: GoogleFonts.rubik(
-  fontSize: 10,
-  fontWeight: FontWeight.w200
-),);
-Text secondSlideTextsub =Text("Revamp your style with our latest arrivals! Enjoy an exclusive 20% off. Elevate your wardrobe with trendy, high-quality pieces. From fashion-forward apparel to the perfect accessories, we have it all. Limited time offer. Shop now and be part of the fashion revolution!",
-style: GoogleFonts.rubik(
-  fontSize: 10,
-  fontWeight: FontWeight.w200
-),);
-Text thirdSlideTextsub =Text("Revamp your style with our latest arrivals! Enjoy an exclusive 20% off. Elevate your wardrobe with trendy, high-quality pieces. From fashion-forward apparel to the perfect accessories, we have it all. Limited time offer. Shop now and be part of the fashion revolution!",
-style: GoogleFonts.rubik(
-  fontSize: 10,
-  fontWeight: FontWeight.w200
-),);
+Text firstSlideTextMain = Text(
+  "20% Discount New Arrival Products",
+  style: GoogleFonts.rubik(fontSize: 30, fontWeight: FontWeight.w500),
+);
+Text secondSlideTextMain = Text(
+  "20% Discount New Arrival Products",
+  style: GoogleFonts.rubik(fontSize: 30, fontWeight: FontWeight.w500),
+);
+Text thirdSlideTextMain = Text(
+  "20% Discount New Arrival Products",
+  style: GoogleFonts.rubik(fontSize: 30, fontWeight: FontWeight.w500),
+);
+Text firstSlideTextsub = Text(
+  "Revamp your style with our latest arrivals! Enjoy an exclusive 20% off. Elevate your wardrobe with trendy, high-quality pieces. From fashion-forward apparel to the perfect accessories, we have it all. Limited time offer. Shop now and be part of the fashion revolution!",
+  style: GoogleFonts.rubik(fontSize: 10, fontWeight: FontWeight.w200),
+);
+Text secondSlideTextsub = Text(
+  "Revamp your style with our latest arrivals! Enjoy an exclusive 20% off. Elevate your wardrobe with trendy, high-quality pieces. From fashion-forward apparel to the perfect accessories, we have it all. Limited time offer. Shop now and be part of the fashion revolution!",
+  style: GoogleFonts.rubik(fontSize: 10, fontWeight: FontWeight.w200),
+);
+Text thirdSlideTextsub = Text(
+  "Revamp your style with our latest arrivals! Enjoy an exclusive 20% off. Elevate your wardrobe with trendy, high-quality pieces. From fashion-forward apparel to the perfect accessories, we have it all. Limited time offer. Shop now and be part of the fashion revolution!",
+  style: GoogleFonts.rubik(fontSize: 10, fontWeight: FontWeight.w200),
+);
+Text termsAndCondition = Text(
+  "By creating an account you have to agree\nwith our terms & condition.",
+  style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w200,),
+);
--- lib/view/signup_page/signup_page.dart
+++ lib/view/signup_page/signup_page.dart
@@ -1,10 +1,12 @@
 import 'package:aiden/utils/colors.dart';
 import 'package:aiden/utils/images.dart';
+import 'package:aiden/utils/texts.dart';
 import 'package:aiden/utils/variables.dart';
 import 'package:aiden/view/widgets/custom_textfield.dart';
 import 'package:aiden/view/widgets/text_widget.dart';
 import 'package:aiden/view/widgets/custome_button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignupPage extends StatefulWidget {
   SignupPage({super.key});
@@ -31,119 +33,130 @@ class _SignupPageState extends State<SignupPage> {
     hight = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: SizedBox(
-          height: hight,
-          width: width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              brandName(30.00, Colors.black, FontWeight.w500),
-              SizedBox(
-                width: width! * .8,
-                height: hight! * .3,
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      customeTextField(
-                      
-                          controller: userNameController,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "enter user name";
-                            }
-                            return null;
-                          },
-                         
-                          labelText: "User Name",
-                          icon: userNameController.text.isNotEmpty
-                              ? const Icon(
-                                  Icons.check_circle_sharp,
-                                  color: lightGreen,
-                                )
-                              : const Icon(
-                                  Icons.check_circle_outline,
-                                  color: grey,
-                                )),
-                      customeTextField(
-                        controller: emailController,
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        margin: EdgeInsets.all(20),
+        height: hight,
+        width: width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            brandName(30.00, Colors.black, FontWeight.w500),
+            ListTile(
+              title: Text(
+                "Sign Up",
+                style: GoogleFonts.poppins(
+                    fontSize: 30, fontWeight: FontWeight.w600),
+              ),
+              subtitle: Text("Create an new account",
+                  style: GoogleFonts.poppins(
+                      fontSize: 20, fontWeight: FontWeight.w400)),
+            ),
+            SizedBox(
+              width: width! * .8,
+              height: hight! * .3,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    customeTextField(
+                        controller: userNameController,
                         validator: (value) {
                           if (value!.isEmpty) {
-                            return "enter email address";
-                          }
-                          if (!RegExp(
-                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                              .hasMatch(value)) {
-                            return "enter correct email address";
+                            return "enter user name";
                           }
                           return null;
                         },
-                        labelText: "Email",
-                      ),
-                      customeTextField(
-                          controller: passwordController,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "enter new password";
-                            }
-                            if (value.length < 8) {
-                              return "password must be  8 digit ";
-                            }
-                            return null;
-                          },
-                          labelText: "Password",
-                          obsecure: isPasswordHide,
-                          icon: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  isPasswordHide = !isPasswordHide;
-                                });
-                              },
-                              icon: isPasswordHide ? hideImage : viewImage)),
-                      customeTextField(
-                          controller: confPasswordController,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "confirm your password";
-                            }
-                            if (value != passwordController.text) {
-                              return "does not match password";
-                            }
-                            return null;
-                          },
-                          labelText: "Confirm Password",
-                          obsecure: isPasswordHide,
-                          icon: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  isPasswordHide = !isPasswordHide;
-                                });
-                              },
-                              icon: isPasswordHide ? hideImage : viewImage)),
-                    ],
-                  ),
+                        labelText: "User Name",
+                        icon: userNameController.text.isNotEmpty
+                            ? const Icon(
+                                Icons.check_circle_sharp,
+                                color: lightGreen,
+                              )
+                            : const Icon(
+                                Icons.check_circle_outline,
+                                color: grey,
+                              )),
+                    customeTextField(
+                      controller: emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "enter email address";
+                        }
+                        if (!RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value)) {
+                          return "enter correct email address";
+                        }
+                        return null;
+                      },
+                      labelText: "Email",
+                    ),
+                    customeTextField(
+                        controller: passwordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "enter new password";
+                          }
+                          if (value.length < 8) {
+                            return "password must be  8 digit ";
+                          }
+                          return null;
+                        },
+                        labelText: "Password",
+                        obsecure: isPasswordHide,
+                        icon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isPasswordHide = !isPasswordHide;
+                              });
+                            },
+                            icon: isPasswordHide ? hideImage : viewImage)),
+                    customeTextField(
+                        controller: confPasswordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "confirm your password";
+                          }
+                          if (value != passwordController.text) {
+                            return "does not match password";
+                          }
+                          return null;
+                        },
+                        labelText: "Confirm Password",
+                        obsecure: isPasswordHide,
+                        icon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isPasswordHide = !isPasswordHide;
+                              });
+                            },
+                            icon: isPasswordHide ? hideImage : viewImage)),
+                  ],
                 ),
               ),
-              Checkbox(
-                  value: isCheckBoxEnabled,
-                  onChanged: (_) {
-                    setState(() {
-                      isCheckBoxEnabled = _!;
-                    });
-                  }),
-              customeButton(
-                  text: "Login",
-                  onpressed: () {
-                    setState(() {
-                      _formKey.currentState!.validate();
-                      if (_formKey.currentState!.validate()) {}
-                    });
-                  },
-                  bgColor: const MaterialStatePropertyAll(black))
-            ],
-          ),
-        ));
+            ),
+            ListTile(
+                leading: Checkbox(
+                    value: isCheckBoxEnabled,
+                    onChanged: (_) {
+                      setState(() {
+                        isCheckBoxEnabled = _!;
+                      });
+                    }),
+                title: termsAndCondition),
+            customeButton(
+                text: "Login",
+                onpressed: () {
+                  setState(() {
+                    if (_formKey.currentState!.validate()) {}
+                  });
+                },
+                bgColor: const MaterialStatePropertyAll(black))
+          ],
+        ),
+      ),
+    );
   }
 }
