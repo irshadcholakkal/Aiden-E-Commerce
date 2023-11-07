91727ad562ef2e84b3c869009cfb313437a81d3b
mohammedakbark <akbarmohd00123@gmail.com>
2023-11-07 11:55:08 +0530

    akbar  upates in all file

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -1,4 +1,4 @@
-import 'package:aiden/view/widgets/brandname_text.dart';
+import 'package:aiden/view/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class Loginscreen extends StatefulWidget {
