b57cb60c2ec901978172e60d1addb24547e6deff
mohammedakbark <akbarmohd00123@gmail.com>
2023-11-13 12:54:10 +0530

    pull

---
+++ lib/view/categories_pages/category_paage.dart
@@ -0,0 +1,24 @@
+import 'package:aiden/view/widgets/custom_nav_bar.dart';
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
+
+  @override
+  State<CategoryPage> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // color: Colors.red,
+        height: double.infinity,
+        width: double.infinity,
+       
+      ),
+      // bottomNavigationBar: navBar(context:context),
+    );
+  }
+}
