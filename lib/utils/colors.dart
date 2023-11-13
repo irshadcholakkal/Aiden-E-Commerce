82a8c7699ea43176ff294845e2e70a04b862534b
mohammedakbark <akbarmohd00123@gmail.com>
2023-11-13 12:47:57 +0530

    gnav

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,3 +4,4 @@ const black = Colors.black;
 const white = Colors.white;
 const lightGreen = Color.fromARGB(255, 44, 179, 65);
 const grey = Color.fromARGB(255, 162, 165, 163);
+final gnavBGColor = Colors.grey.shade200;
