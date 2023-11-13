8255aa6143bd4336b5251346118108ee54d31f64
mohammedakbark <akbarmohd00123@gmail.com>
2023-11-13 13:03:20 +0530

    add gnav in home page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:aiden/view/forgot_page/forgotpage.dart';
 import 'package:aiden/view/home_page/home_page.dart';
 import 'package:aiden/view/login_screen/login_screen.dart';
 import 'package:aiden/view/signup_page/signup_page.dart';
+import 'package:aiden/view/splash_screen/splash_screen.dart';
 
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(debugShowCheckedModeBanner: false,
-     home: HomePage());
+     home: SplashScreen());
   }
 }
