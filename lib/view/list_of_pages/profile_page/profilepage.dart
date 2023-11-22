1bcc91f8017de02fc1b7b57102c024833b0fb128
muhammed dilshad <jinudilshad812@gmail.com>
2023-11-22 10:44:24 +0530

    vrious changes

--- lib/view/list_of_pages/profile_page/profilepage.dart
+++ lib/view/list_of_pages/profile_page/profilepage.dart
@@ -2,7 +2,6 @@ import 'package:aiden/utils/colors.dart';
 import 'package:aiden/view/list_of_pages/profile_page/widget/fav.dart';
 import 'package:aiden/view/list_of_pages/profile_page/widget/listtile.dart';
 import 'package:aiden/view/list_of_pages/profile_page/pages_in_profile/settings.dart';
-import 'package:aiden/view/widgets/custom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
 class Profilepage extends StatefulWidget {
@@ -21,105 +20,127 @@ class _ProfilepageState extends State<Profilepage> {
         width: MediaQuery.of(context).size.width,
         color: Colors.white,
         child: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                height: 60,
-                width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(color: white),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Image.asset("assets/images/backog.png")),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Image.asset("assets/images/settingsog.png")),
-                    ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(color: white),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Image.asset("assets/images/backog.png")),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Image.asset("assets/images/settingsog.png")),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 10, bottom: 10, left: 20, right: 20),
-                child: Container(
+                const SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10, bottom: 10, left: 20, right: 20),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: const ListTile(
+                        leading: SizedBox(),
+                        title: Text("name"),
+                        subtitle: Text("email"),
+                      )),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10, bottom: 10, left: 20, right: 20),
+                  child: Container(
                     decoration: BoxDecoration(
                         border: Border.all(color: grey),
                         borderRadius: BorderRadius.circular(20)),
-                    child: const ListTile(
-                      leading: SizedBox(),
-                      title: Text("name"),
-                      subtitle: Text("email"),
-                    )),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 10, bottom: 10, left: 20, right: 20),
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: grey),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Column(
-                    children: [
-                      listtile(
-                          text: "Personal details",
-                          leading: const Icon(Icons.abc),
-                          ontap: () {}),
-                      listtile(
-                          text: "My Orders",
-                          leading: const Icon(Icons.abc),
-                          ontap: () {}),
-                      listtile(
-                          text: "My Favourites",
+                    child: Column(
+                      children: [
+                        listtile(
+                            text: "Personal details",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {}),
+                        listtile(
+                            text: "My Orders",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {}),
+                        listtile(
+                            text: "My Favourites",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => FavPage()));
+                            }),
+                        listtile(
+                            text: "Shipping Address",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {}),
+                        listtile(
+                            text: "My Card",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {}),
+                        listtile(
+                          text: " Settings",
                           leading: const Icon(Icons.abc),
                           ontap: () {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => FavPage()));
-                          }),
-                      listtile(
-                          text: "Shipping Address",
-                          leading: const Icon(Icons.abc),
-                          ontap: () {}),
-                      listtile(
-                          text: "My Card",
-                          leading: const Icon(Icons.abc),
-                          ontap: () {}),
-                      listtile(
-                        text: " Settings",
-                        leading: const Icon(Icons.abc),
-                        ontap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Settings()));
-                        },
-                      )
-                    ],
+                                    builder: (context) => Settings()));
+                          },
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                child: Column(
-                  children: [],
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10, bottom: 10, left: 20, right: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: grey),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                      children: [
+                        listtile(
+                            text: " FAQs",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {}),
+                        listtile(
+                            text: "Privacy Policy",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {}),
+                        listtile(
+                            text: "Documents",
+                            leading: const Icon(Icons.abc),
+                            ontap: () {}),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-              Container(),
-            ],
+                
+              ],
+            ),
           ),
         ),
       ),
-      // bottomNavigationBar: navBar(context: context),
+      //  bottomNavigationBar: navBar(context: context),
     );
   }
 }
